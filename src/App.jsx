ddfad1fe0a2279e041c75a1956549ff847a1ebc4
zahra987 <zhr.ramezanpour@gmail.com>
2023-05-18 14:54:26 +0330

    react-router-dom installed and routes added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Home from './Components/Routes/home/Home'
+import Store from './Components/Routes/store/Store'
+import Cart from './Components/Routes/cart/Cart'
+import Orders from './Components/Routes/orders/Orders'
+import Saved from './Components/Routes/saved/Saved'
+import AboutUs from './Components/Routes/aboutUs/AboutUs'
+import ContactUs from './Components/Routes/contactUs/ContactUs'
+import Login from './Components/Routes/login/Login'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>app</div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Navigate replace to="/" />} />
+        <Route path='/store' element={<Store/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/orders' element={<Orders/>} />
+        <Route path='/saved' element={<Saved/>} />
+        <Route path='/aboutus' element={<AboutUs/>} />
+        <Route path='/contactus' element={<ContactUs/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
