67c3895018b8d6766fa2621786ffdccb456a1b99
zahra987 <zhr.ramezanpour@gmail.com>
2023-05-18 17:12:48 +0330

    header is ready for all devices and Link changed to NavLink

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, {useRef} from 'react'
 import './header.css'
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import {AiOutlineHome} from 'react-icons/ai'
 import {FaStoreAlt} from 'react-icons/fa'
 import {AiOutlineShoppingCart} from 'react-icons/ai'
@@ -24,14 +24,14 @@ export default function Header() {
       <nav className='nav-right close' ref={navRightRef}>
         <ul>
           <li id='close-x'><span onClick={handleClick}>X</span></li>
-          <Link to='/'><li><AiOutlineHome/><span>HOME</span></li></Link>
-          <Link to='/store'><li><FaStoreAlt/><span>STORE</span></li></Link>
-          <Link to='/cart'><li><AiOutlineShoppingCart/><span>CART</span></li></Link>
-          <Link to='/orders'><li><AiOutlineUnorderedList/><span>ORDERS</span></li></Link>
-          <Link to='/saved'><li><HiOutlineSave/><span>SAVED</span></li></Link>
-          <Link to='/aboutus'><li><FcAbout/><span>ABOUTUS</span></li></Link>
-          <Link to='/contactus'><li><MdConnectWithoutContact/><span>CONTACTUS</span></li></Link>
-          <Link to='/login'><li><MdLogin/><span>LOGIN</span></li></Link>
+          <NavLink style={({isActive}) =>{return {backgroundColor:isActive?"rgb(103,132,226)":"#7d6fce"}}} to='/'><li><AiOutlineHome/><span>HOME</span></li></NavLink>
+          <NavLink style={({isActive}) =>{return {backgroundColor:isActive?"rgb(103,132,226)":"#7d6fce"}}} to='/store'><li><FaStoreAlt/><span>STORE</span></li></NavLink>
+          <NavLink style={({isActive}) =>{return {backgroundColor: isActive?"rgb(103,132,226)":"#7d6fce"}}} to='/cart'><li><AiOutlineShoppingCart/><span>CART</span></li></NavLink>
+          <NavLink style={({isActive}) =>{return {backgroundColor: isActive?"rgb(103,132,226)":"#7d6fce"}}} to='/orders'><li><AiOutlineUnorderedList/><span>ORDERS</span></li></NavLink>
+          <NavLink style={({isActive}) =>{return {backgroundColor: isActive?"rgb(103,132,226)":"#7d6fce"}}} to='/saved'><li><HiOutlineSave/><span>SAVED</span></li></NavLink>
+          <NavLink style={({isActive}) =>{return {backgroundColor: isActive?"rgb(103,132,226)":"#7d6fce"}}} to='/aboutus'><li><FcAbout/><span>ABOUTUS</span></li></NavLink>
+          <NavLink style={({isActive}) =>{return {backgroundColor: isActive?"rgb(103,132,226)":"#7d6fce"}}} to='/contactus'><li><MdConnectWithoutContact/><span>CONTACTUS</span></li></NavLink>
+          <NavLink style={({isActive}) =>{return {backgroundColor: isActive?"rgb(103,132,226)":"#7d6fce"}}} to='/login'><li><MdLogin/><span>LOGIN</span></li></NavLink>
         </ul>
       </nav>
     </header>
